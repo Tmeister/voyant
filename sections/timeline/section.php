c5c2593916c1936db26af10d0f96f46c58bdc84b
Enrique Chavez <noone@tmeister.net>
2014-03-28 13:11:23 -0600

    Timeline Alignment left/right/center

--- sections/timeline/section.php
+++ sections/timeline/section.php
@@ -25,43 +25,79 @@ class TmVoyantTimeline extends PageLinesSection {
 			<div class="span12">
 				<ul class="timeline">
 				    <li>
-				        <time class="time" datetime="2013-04-10 18:30"><span>4/10/13</span> <span>18:30</span></time>
 				        <div class="icon icon-phone"></div>
 				        <div class="label-box">
 				            <div class="title">Ricebean black-eyed pea</div>
-				            <div class="details">Winter purslane...</div>
+				            <div class="details">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.</div>
 				        </div>
 				    </li>
 				    <li>
-				        <time class="time" datetime="2013-04-11T12:04"><span>4/11/13</span> <span>12:04</span></time>
 				        <div class="icon icon-gear"></div>
 				        <div class="label-box">
 				            <div class="title">Greens radish arugula</div>
-				            <div class="details">Caulie dandelion maize...</div>
+				            <div class="details">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.</div>
 				        </div>
 				    </li>
 				    <li>
-				        <time class="time" datetime="2013-04-13 05:36"><span>4/13/13</span> <span>05:36</span></time>
 				        <div class="icon icon-music"></div>
 				        <div class="label-box">
 				            <div class="title">Sprout garlic kohlrabi</div>
-				            <div class="details">Parsnip lotus root...</div>
+				            <div class="details">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.</div>
 				        </div>
 				    </li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="span12">
+				<ul class="timeline align-left">
 				    <li>
-				        <time class="time" datetime="2013-04-15 13:15"><span>4/15/13</span> <span>13:15</span></time>
 				        <div class="icon icon-phone"></div>
 				        <div class="label-box">
-				            <div class="title">Watercress ricebean</div>
-				            <div class="details">Peanut gourd nori...</div>
+				            <div class="title">Ricebean black-eyed pea</div>
+				            <div class="details">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.</div>
 				        </div>
 				    </li>
 				    <li>
-				        <time class="time" datetime="2013-04-16 21:30"><span>4/16/13</span> <span>21:30</span></time>
 				        <div class="icon icon-gear"></div>
 				        <div class="label-box">
-				            <div class="title">Courgette daikon</div>
-				            <div class="details">Parsley amaranth tigernut...</div>
+				            <div class="title">Greens radish arugula</div>
+				            <div class="details">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.</div>
+				        </div>
+				    </li>
+				    <li>
+				        <div class="icon icon-music"></div>
+				        <div class="label-box">
+				            <div class="title">Sprout garlic kohlrabi</div>
+				            <div class="details">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.</div>
+				        </div>
+				    </li>
+				</ul>
+			</div>
+		</div>
+		<div class="row">
+			<div class="span12">
+				<ul class="timeline align-right">
+				    <li>
+				        <div class="icon icon-phone"></div>
+				        <div class="label-box">
+				            <div class="title">Ricebean black-eyed pea</div>
+				            <div class="details">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.</div>
+				        </div>
+				    </li>
+				    <li>
+				        <div class="icon icon-gear"></div>
+				        <div class="label-box">
+				            <div class="title">Greens radish arugula</div>
+				            <div class="details">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.</div>
+				        </div>
+				    </li>
+				    <li>
+				        <div class="icon icon-music"></div>
+				        <div class="label-box">
+				            <div class="title">Sprout garlic kohlrabi</div>
+				            <div class="details">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.</div>
 				        </div>
 				    </li>
 				</ul>
